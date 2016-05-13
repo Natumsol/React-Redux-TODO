212fe4f6444ed409839613a758cdc0671c9e937b
natumsol <natumsol@gmail.com>
2016-05-13 15:39:39 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import todoApp from './actions/reducers';
+import todoApp from './reducers';
 
 let store = createStore(todoApp);
 
